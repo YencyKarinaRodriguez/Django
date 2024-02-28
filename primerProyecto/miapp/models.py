51d2626c9adb9ba79ccf0d7b6367d470b51745db
YencyKarinaRodriguez <yency072005@gmail.com>
2024-02-28 11:57:28 -0500

    models

--- primerProyecto/miapp/models.py
+++ primerProyecto/miapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    contect = models.TextField()
+    public = models.BooleanField()
+    create_date = models.DateTimeField(auto_now_add = True)
+    update_date = models.DateField(auto_now = True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    create_date = models.DateField()

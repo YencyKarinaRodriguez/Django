03f034db310d50817b197ddb13bee2059fe3d735
unknown <yency072005@gmail.com>
2024-03-22 09:52:57 -0500

    22 de marzo

---
+++ primerProyecto/pages/context_processors.py
@@ -0,0 +1,7 @@
+from pages.models import Page
+
+def get_pages(request):
+    pages = Page.objects.values_list('title', flat = True)
+    return{
+        'pages':pages
+    }
---
+++ primerProyecto/pages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    slug = models.CharField(unique = True, max_length=150, verbose_name="Url Amigable")#Url que va a tener la pagina
+    visible = models.BooleanField(verbose_name="¿Visible?")
+    create_pg = models.DateTimeField(auto_now_add = True, verbose_name="Creado el")
+    update_pg = models.DateTimeField(auto_now = True, verbose_name="Actualizado el")
+
+    class Meta:
+        verbose_name = "Pagina"
+        verbose_name_plural = "Paginas"
+
+    def __str__(self):
+            return f"{self.title}- Url Amigable: {self.slug}"
